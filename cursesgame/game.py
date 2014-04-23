f203b8cad7ba3155996206a0bc6fb5e3be36e837
Steven <theepicsnail@gmail.com>
2014-04-23 01:17:42 -0400

    Unused import and whitespace

--- cursesgame/game.py
+++ cursesgame/game.py
@@ -1,12 +1,11 @@
 import curses, curses.wrapper
 import locale
 
-locale.setlocale(locale.LC_ALL,"")
+locale.setlocale(locale.LC_ALL, "")
 curses.initscr()
 curses.start_color()
 curses.curs_set(0)
 
-from util import color
 import engine
 
 def main(screen):
